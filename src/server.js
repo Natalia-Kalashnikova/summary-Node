0bd4c7174eef0d1e6f3eb7b4034b265f1ee1c148
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-19 02:23:04 +0300

    Add basic Express server configuration

--- src/server.js
+++ src/server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
+import { env } from './utils/env';
 
 
-const PORT = 3000;
+const PORT = Number(env('PORT', '3000'));
 
 export const startServer = () => {
     const app = express();
+
+    app.use(express.json());
+
     app.use(cors());
 
     app.use(
@@ -17,36 +21,28 @@ export const startServer = () => {
         }),
     );
 
-    app.use((req, res, next) => {
-        console.log(`Time: ${new Date().toLocaleString()}`);
-        next();
-    });
+    // app.use((req, res, next) => {
+    //     console.log(`Time: ${new Date().toLocaleString()}`);
+    //     next();
+    // });
 
-    app.use(express.json());
-
+    
     app.get('/', (req, res) => {
         res.json({ message: 'Hello world!' });
     });
 
-    app.use((req, res, next, err) => {
-        res.status(500).json({
-            message: 'Something went wrong',
-        });
-    });
-
-    app.use('*', (req, res, next) => {
+        app.use('*', (req, res, next) => {
         res.status(404).json({
             message: 'Not found',
         });
     });
 
-    app.use((err, req, res, next) => {
+    app.use((req, res, next, err) => {
         res.status(500).json({
             message: 'Something went wrong',
             error: err.message,
         });
     });
-
 
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
