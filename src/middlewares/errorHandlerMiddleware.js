eea29fa7377a0f3fc5cda212d7370f84fde89eb6
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-30 03:08:54 +0300

    changed errorHandlerMiddleware

--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,5 +1,11 @@
 export const errorHandlerMiddleware = (error, req, res, next) => {
-  res.status(500).send(error.message);
+  res.status(500).json({
+    status: 500,
+    message: "Internal server error",
+    data: {
+      message: error.message,
+    }
+  });
 };
 
 // import { HttpError } from 'http-errors';
