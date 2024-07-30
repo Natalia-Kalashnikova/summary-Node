d90675444e2062498c0cab6572a1e2afe56b8882
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-07-30 03:43:03 +0300

    changed errorHandlerMiddleware

--- src/controllers/students.js
+++ src/controllers/students.js
@@ -14,13 +14,7 @@ export const getStudentByIdController = async (req, res) => {
     const id = req.params.studentId;
     const student = await getStudentById(id);
 
-    if (!student) {
-        return res.status(404).json({
-            status: 404,
-            message: `Student with id ${id} not found!`,
-        });
-    }
-
+    
     res.json({
         status: 200,
         message: `Successfully get student with id ${id}!`,
--- src/middlewares/errorHandlerMiddleware.js
+++ src/middlewares/errorHandlerMiddleware.js
@@ -1,4 +1,13 @@
+import {HttpError} from "http-errors";
+
 export const errorHandlerMiddleware = (error, req, res, next) => {
+  if (error instanceof HttpError) {
+    res.status(error.status).json({
+    status: error.status,
+    message: error.message,
+  });
+}
+
   res.status(500).json({
     status: 500,
     message: "Internal server error",
