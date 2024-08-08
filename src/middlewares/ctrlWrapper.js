811437fc0f8e08e6f3f028e776133989ff44a4ba
Natalia-Kalashnikova <144282173+Natalia-Kalashnikova@users.noreply.github.com>
2024-08-08 19:06:27 +0300

    Update

--- src/middlewares/ctrlWrapper.js
+++ src/middlewares/ctrlWrapper.js
@@ -6,12 +6,3 @@ export const ctrWrapper = (controller) =>async(req, res, next)=> {
     }
 };
 
-// export const ctrWrapper = (controller) => {
-//     return async (req, res, next) => {
-//         try {
-//             await controller(req, res, next);
-//         } catch (err) {
-//             next(err);
-//         }
-//     };
-// };
--- src/services/students.js
+++ src/services/students.js
@@ -22,7 +22,13 @@
 //   if (filter.maxAge) {
 //     studentsQuery.where('age').lte(filter.maxAge);
 //   }
+//   if (filter.minAge) {
+//     studentsQuery.where('age').gte(filter.minAge);
+//   }
 //   if (filter.maxAvgMark) {
+//     studentsQuery.where('avgMark').gte(filter.maxAvgMark);
+//   }
+//   if (filter.minAvgMark) {
 //     studentsQuery.where('avgMark').gte(filter.minAvgMark);
 //   }
 
@@ -70,48 +76,6 @@
 //     throw createHttpError(404, 'Student not found');
 //   }
 //   return {
-//     student: rawResult.value,
-//     isNew: !rawResult?.lastErrorObject?.updatedExisting,
-//   };
-// };
-
-// export const deleteStudentById = async (studentId) => {
-//   await Student.findByIdAndDelete(studentId);
-// };
-
-// import createHttpError from 'http-errors';
-// import { Student } from '../db/models/student.js';
-
-
-// export const getAllStudents = async () => {
-//     return await Student.find();
-// };
-// export const getStudentById = async (id) => {
-//   const student = await Student.findById(id);
-//   if (!student) {
-//     throw createHttpError(404, 'Student not found');
-//   }
-//   return student;
-// };
-
-// export const createStudent = async (payload) => {
-//   const student = await Student.create(payload);
-
-//   return student;
-// };
-
-// export const upsertStudent = async (id, payload, options = {}) => {
-//   const rawResult = await Student.findByIdAndUpdate(id, payload, {
-//     new: true,
-//     includeResultMetadata: true,
-//     ...options,
-//   });
-
-//   if (!rawResult || rawResult.value) {
-//     throw createHttpError(404, 'Student not found');
-//   }
-
-//     return {
 //     student: rawResult.value,
 //     isNew: !rawResult?.lastErrorObject?.updatedExisting,
 //   };
